404846532950b05c1f227ccceb06abbacaab6e5a
AndrewH-ML <40874538+AndrewH707@users.noreply.github.com>
2024-09-13 23:23:55 -0700

    Made important changes by organizing environment, setting up reproducable cluster-config, and configured to be used with image uploaded to ECR

--- service/config.py
+++
@@ -1,8 +0,0 @@
-# import os
-
-# class Config:
-#     S3_BUCKET = os.environ.get('S3_BUCKET')
-#     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-#     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-#     AWS_REGION = os.environ.get('AWS_REGION', 'us-east-1')
-#     SECRET_KEY = os.environ.get('SECRET_KEY', '')
