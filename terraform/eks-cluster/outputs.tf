3f4de4d162b7d0adea1fe7647de924cdb70bcdf1
AndrewH-ML <40874538+AndrewH707@users.noreply.github.com>
2024-10-15 09:09:32 -0700

    added alb url to terraform outputs

--- terraform/eks-cluster/outputs.tf
+++ terraform/eks-cluster/outputs.tf
@@ -9,3 +9,8 @@ output "cluster_name" {
     description = "EKS cluster name"
     value       = module.eks.cluster_name
 }
+
+output "alb_dns_name" {
+    description = "The DNS name of the Application Load Balancer"
+    value       = aws_lb.alb.dns_name
+}
